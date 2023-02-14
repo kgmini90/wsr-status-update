6349d7dade554c6e3163d6f21d5cefac3f41dc76
karthik <karthikganesh.vellaichami@fisglobal.com>
2023-02-14 15:31:55 +0530

    trial again with URL path correction

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { AppComponent } from './app.component';
 
 const routes: Routes = [
   { path: '', component: AppComponent },
-  { path: 'wsr', component: AppComponent },
+  { path: '/', component: AppComponent },
   { path: '', redirectTo: '/', pathMatch: 'full' },
   { path: '**', redirectTo: '/', pathMatch: 'full' },
 ];
