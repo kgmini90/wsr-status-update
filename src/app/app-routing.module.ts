d1ee46aecac6237521d02d84ff6dcfeb8db49a5c
karthik <karthikganesh.vellaichami@fisglobal.com>
2023-02-14 14:57:30 +0530

    URL path change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 
-const routes: Routes = [{ path: '**', component: AppComponent }];
+const routes: Routes = [
+  { path: 'app-root', component: AppComponent },
+  { path: '', redirectTo: '/app-root', pathMatch: 'full' },
+  { path: '**', redirectTo: '/app-root', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
